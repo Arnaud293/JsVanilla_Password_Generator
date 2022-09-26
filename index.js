24357d2c4847aa4ef624f4f2b9169f5f17114970
Arnaud293 <arnaudlt293@gmail.com>
2022-09-26 16:54:19 +0200

    add function event and data for password generation

---
+++ index.js
@@ -0,0 +1,12 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toLocaleUpperCase();
+const dataNumbers = '0123456789';
+const dataSymbols ="^¨?.ù:;&$éè'\"()-_ç";
+
+
+
+const generatePassword = () => {
+    console.log('later');
+}
+
+generateButton.addEventListener('click', generatePassword)
